01d1879eeed7cbb7feb5d4e5ecb67e7da5e57582
J Ade <joshua.adegbola20@gmail.com>
2024-05-01 14:45:33 +0100

    push the cacstld first draft

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("navbar").classList.add("scrolled");
+    } else {
+        document.getElementById("navbar").classList.remove("scrolled");
+    }
+}
